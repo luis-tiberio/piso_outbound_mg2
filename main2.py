4a30b532bf3832dc13da68234bda037278de5623
luis-tiberio <luisotaviotiberio@gmail.com>
2025-06-14 05:04:02 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -55,7 +55,7 @@ async def main():
             await page.wait_for_selector('//*[@placeholder="Ops ID"]', timeout=15000)
             await page.locator('//*[@placeholder="Ops ID"]').fill('Ops35683')
             await page.locator('//*[@placeholder="Senha"]').fill('@Shopee123')
-            await page.locator('/html[1]/body[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[3]/form[1]/div[1]/div[1]/button[1]/div[1]').click()
+            await page.locator('/html/body/div[1]/div/div[2]/div/div/div[1]/div[3]/form/div/div/button/div').click()
             await page.wait_for_timeout(15000)
             try:
                 await page.locator('.ssc-dialog-close').click(timeout=5000)
